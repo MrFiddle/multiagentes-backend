ad509d3ec4f0a5442d7b96c0cdb51fc8a88c2590
Juan Pablo <jppd0657@gmail.com>
2022-11-17 10:34:07 -0600

    random positions everytime you make a request

--- back.py
+++ back.py
@@ -1,33 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import random
 import json
+from flask import Flask, request, jsonify, send_file
 
-class Car:
-    def __init__(self, x, y, z) -> None:
-        self.x = x
-        self.y = y
-        self.z = z
+app = Flask(__name__)
+
+if __name__ == '__main__':
+
+
+    class Car:
+        def __init__(self, id, x, y, z) -> None:
+            self.id = id
+            self.x = x
+            self.y = y
+            self.z = z
+        
+        def getX(self):
+            return self.x
+        
+        def getY(self):
+            return self.y
+
+        def getZ(self):
+            return self.z
+        
+        def getId(self):
+            return self.id
+
+    @app.route('/cars', methods=['GET'])
+    def getCars():
+
+        cars = []
+        carNumber = 20
+        counter = 1
+
+        for i in range(carNumber):
+
+            x = random.uniform(0, 100)
+            z = random.uniform(0, 100)
+            cars.append(Car("car" + str(counter),x, 0, z))
+            counter = counter + 1
+
+            print(counter)
+
+        def createJson():
+            with open('cars.json', 'w') as f:
+                json.dump(cars, f, default=lambda o: o.__dict__, indent=4)
+        
+        createJson()
+        return send_file('cars.json')
     
-    def getX(self):
-        return self.x
-    
-    def getY(self):
-        return self.y
-
-    def getZ(self):
-        return self.z
-
-    
-cars = []
-carNumber = 20
-
-for i in range(carNumber):
-    x = random.uniform(0, 100)
-    # y = random.uniform(0, 100)
-    z = random.uniform(0, 100)
-    cars.append(Car(x, 0, z))
-
-for i in cars:
-    print("x: " + str(i.getX()), "y: " + str(i.getY()), "z: " + str(i.getZ()))
-
-with open('cars.json', 'w') as f:
-    json.dump(cars, f, default=lambda o: o.__dict__, indent=4)
+    app.run(debug=True, port=8000)
