1e80741bf06ad1f55b094192df87a46c9f076360
Juan Pablo <jppd0657@gmail.com>
2022-11-21 15:39:31 -0600

    cars moving

--- back.py
+++ back.py
@@ -8,47 +8,53 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
 
-
     class Car:
-        def __init__(self, id, x, y, z) -> None:
+        def __init__(self, id, initialPosition) -> None:
             self.id = id
-            self.x = x
-            self.y = y
-            self.z = z
-        
-        def getX(self):
-            return self.x
-        
-        def getY(self):
-            return self.y
+            self.initialPosition = initialPosition
 
-        def getZ(self):
-            return self.z
+            self.coordinates = []
+            self.coordinates.append(initialPosition)       
         
         def getId(self):
             return self.id
-
-    @app.route('/cars', methods=['GET'])
-    def getCars():
-
-        cars = []
-        carNumber = 20
-        counter = 1
-
-        for i in range(carNumber):
-
-            x = random.uniform(0, 100)
-            z = random.uniform(0, 100)
-            cars.append(Car("car" + str(counter),x, 0, z))
-            counter = counter + 1
-
-            print(counter)
-
-        def createJson():
-            with open('cars.json', 'w') as f:
-                json.dump(cars, f, default=lambda o: o.__dict__, indent=4)
         
-        createJson()
-        return send_file('cars.json')
+        def getCoordinates(self):
+            return self.coordinates
+        
+        def getInitialPosition(self):
+            print(self.coordinates)
+            return initialPosition
+        
+        def addCoordinates(self, coordinates):
+            self.coordinates.append(coordinates)
+        
     
-    app.run(debug=True, port=8000)
+    initialPosition = [34, 10, 4]
+    car = Car(1, initialPosition)
+    print(car.getInitialPosition)
+
+    # @app.route('/cars', methods=['GET'])
+    # def getCars():
+
+    #     cars = []
+    #     carNumber = 20
+    #     counter = 1
+
+    #     for i in range(carNumber):
+
+    #         x = random.uniform(0, 100)
+    #         z = random.uniform(0, 100)
+    #         cars.append(Car("car" + str(counter),x, 0, z))
+    #         counter = counter + 1
+
+    #         print(counter)
+
+    #     def createJson():
+    #         with open('cars.json', 'w') as f:
+    #             json.dump(cars, f, default=lambda o: o.__dict__, indent=4)
+        
+    #     createJson()
+    #     return send_file('cars.json')
+    
+    # app.run(debug=True, port=8000)
